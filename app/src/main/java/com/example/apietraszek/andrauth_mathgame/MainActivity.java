06262b068296c9aee4a74dbf23da812fe651b8eb
a.pietraszek <ar.pie.86+github@gmail.com>
2018-02-23 16:06:22 +0100

    added timer and empty answer check.

--- app/src/main/java/com/example/apietraszek/andrauth_mathgame/MainActivity.java
+++ app/src/main/java/com/example/apietraszek/andrauth_mathgame/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.apietraszek.andrauth_mathgame;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import java.util.Random;
 
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView Number2;
     private EditText Attempt;
     private TextView Answer;
+    private TextView TimerText;
+    private boolean gameOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +31,36 @@ public class MainActivity extends AppCompatActivity {
         Number2 = findViewById(R.id.number2);
         Attempt = findViewById(R.id.attempt);
         Answer = findViewById(R.id.answer);
+        TimerText = findViewById(R.id.timerText);
 
         setNewNumbers();
+
+        gameOn = true;
+
+        new CountDownTimer(60000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                TimerText.setText("time remaining: " + millisUntilFinished / 1000);
+            }
+            public void onFinish() {
+                TimerText.setText("done!");
+                gameOn = false;
+                Attempt.setVisibility(View.GONE);
+            }
+        }.start();
     }
 
     public void onSubmitClick (View view){
-        int userAnswer = Integer.parseInt(Attempt.getText().toString());
-        if(userAnswer == value1+value2) {
-            Answer.setText("Correct!");
+        if(!gameOn) return;
 
+        if(Attempt.getText().toString().matches("")){
+            //Context context = getApplicationContext();
+            Toast.makeText(this, "Answer is missing!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        int userAnswer = Integer.parseInt(Attempt.getText().toString());
+        if(value1+value2 == userAnswer) {
+            Answer.setText("Correct!");
         } else {
             Answer.setText("Wrong, the correct answer was: " + (value1+value2));
         }
